04105b74a6ea4bffd8a4268f57a64c6c436b827a
Maklolaj <m.zelek@op.pl>
2022-06-02 09:37:09 +0200

    add schema

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { from } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/demow')],
   controllers: [AppController],
   providers: [AppService],
 })
