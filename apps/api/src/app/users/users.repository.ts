9d1f0299ac6980e005e22879d8e2e85134a534e2
Maklolaj <m.zelek@op.pl>
2022-06-03 23:19:28 +0200

    add user repo

---
+++ apps/api/src/app/users/users.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { FilterQuery, Model } from "mongoose";
+import { User, UserDocument } from "./schemas/users.schema";
+
+@Injectable()
+export class UserRepository {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+    async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
+        return this.userModel.findOne(userFilterQuery);
+    }
+
+    async create(user: User): Promise<User> {
+        const newUser = new this.userModel(user);
+        return newUser.save()
+    }
+}
