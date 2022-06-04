dc31accbd05f7a1f731a065171dda38cbae92b90
Maklolaj <m.zelek@op.pl>
2022-06-04 18:21:28 +0200

    add product service

---
+++ libs/products/src/lib/services/products/products.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '@geometry-shop/data-models'
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root',
+})
+export class ProductsService {
+
+    constructor(private httpClient:HttpClient) {}
+
+    getAllProducts(): Observable<Product[]> {
+        return this.httpClient.get<Product[]>('http://localhost:4200/api/products');
+    }
+}
