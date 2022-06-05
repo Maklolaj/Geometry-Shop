ddc334d2ab80eb5de72a2a8587095e94965ca35a
Maklolaj <m.zelek@op.pl>
2022-06-05 22:46:02 +0200

    add state

--- libs/products/src/lib/state/products/product.reducer.ts
+++ libs/products/src/lib/state/products/product.reducer.ts
@@ -1,11 +1,11 @@
 import { Product } from "@geometry-shop/data-models";
 import { createReducer, on } from "@ngrx/store";
 import { ProductActions } from './product-action-types'
+import {createEntityAdapter, EntityState} from '@ngrx/entity';
 
-
-export const initialProductsState: ReadonlyArray<Product> = []
+export const initialProductsState: Product[] = []
 
 export const productsReducer = createReducer(
     initialProductsState,
-    on(ProductActions.retrieveProductList, (state, {products}) => products)
+    on(ProductActions.retrieveProductList, (state, {products} ) => products )
 )
