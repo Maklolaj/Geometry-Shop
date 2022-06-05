5b3d6aef51a682222b6404f433d45d5f3f29e9bb
Maklolaj <m.zelek@op.pl>
2022-06-05 22:46:55 +0200

    TODO fix invalid object ref

--- libs/products/src/lib/components/product-list/product-list.component.ts
+++ libs/products/src/lib/components/product-list/product-list.component.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import {
   FixedSizeVirtualScrollStrategy,
   VIRTUAL_SCROLL_STRATEGY,
 } from '@angular/cdk/scrolling';
+import { Product } from '@geometry-shop/data-models';
+import { select, Store } from '@ngrx/store';
+import { selectAllProducts } from '../../state/products/product.selectors';
+import { Observable, of } from 'rxjs';
+import { ProductState } from '../../state/products/product.state';
 export class CustomVirtualScrollStrategy extends FixedSizeVirtualScrollStrategy {
   constructor() {
     super(50, 250, 500);
@@ -18,6 +23,20 @@ export class CustomVirtualScrollStrategy extends FixedSizeVirtualScrollStrategy 
     { provide: VIRTUAL_SCROLL_STRATEGY, useClass: CustomVirtualScrollStrategy },
   ],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   items = Array.from({ length: 40 }).map((_, i) => `Item #${i}`);
+
+  products:Product[] = []
+ 
+  constructor(private store:Store) {}
+
+
+  products$: Observable<ProductState> = of();
+
+  ngOnInit() {
+    this.products$ = this.store.pipe(select(selectAllProducts));
+    this.products$.subscribe( a => console.log(a))
+    console.log('xd')
+  }
+  
 }
