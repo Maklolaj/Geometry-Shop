929e75cbd4df0d7bedb1efbe47a2cf2264463455
Maklolaj <m.zelek@op.pl>
2022-06-11 10:54:10 +0200

    entity approache

--- libs/products/src/lib/state/products/product.reducer.ts
+++ libs/products/src/lib/state/products/product.reducer.ts
@@ -9,3 +9,36 @@ export const productsReducer = createReducer(
     initialProductsState,
     on(ProductActions.retrieveProductList, (state, {products} ) => products )
 )
+
+
+export interface CoursesState extends EntityState<Product> {
+    allProductsLoaded: boolean
+}
+
+
+export const adapter = createEntityAdapter<Product>({
+    //sortComparer: compareCourses
+});
+
+
+export const initialCoursesState = adapter.getInitialState({
+    allProductsLoaded:false
+});
+
+
+export const coursesReducer = createReducer(
+
+    initialCoursesState,
+
+    on(ProductActions.retrieveProductList,
+        (state, action) => adapter.setAll( // getAll
+            action.products,
+            {...state,
+                allProductsLoaded:true
+            })),
+);
+
+
+export const {
+    selectAll
+} = adapter.getSelectors();
