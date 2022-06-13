3a5c34e6e6b592a29e623febaac39be6249f8fa7
Maklolaj <m.zelek@op.pl>
2022-06-13 20:34:03 +0200

    ngrx entity test

--- libs/products/src/lib/state/products/product.reducer.ts
+++ libs/products/src/lib/state/products/product.reducer.ts
@@ -1,44 +1,52 @@
-import { Product } from "@geometry-shop/data-models";
-import { createReducer, on } from "@ngrx/store";
-import { ProductActions } from './product-action-types'
-import {createEntityAdapter, EntityState} from '@ngrx/entity';
+import { Product } from '@geometry-shop/data-models';
+import { createReducer, on, State } from '@ngrx/store';
+import { ProductActions } from './product-action-types';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 
-export const initialProductsState: Product[] = []
+// export const initialProductsState: Product[] = [];
 
-export const productsReducer = createReducer(
-    initialProductsState,
-    on(ProductActions.retrieveProductList, (state, {products} ) => products )
-)
+// export const productsReducer = createReducer(
+//   initialProductsState,
+//   on(ProductActions.retrieveProductList, (state, { products }) => products)
+// );
 
-
-export interface CoursesState extends EntityState<Product> {
-    allProductsLoaded: boolean
+export interface ProductsState extends EntityState<Product> {
+  allProductsLoaded: boolean;
 }
 
-
-export const adapter = createEntityAdapter<Product>({
-    //sortComparer: compareCourses
+export const adapter: EntityAdapter<Product> = createEntityAdapter<Product>({
+  selectId: (product: Product) => product.productId,
+  sortComparer: compareProducts,
 });
 
-
-export const initialCoursesState = adapter.getInitialState({
-    allProductsLoaded:false
+export const initialProductsState: ProductsState = adapter.getInitialState({
+  allProductsLoaded: false,
 });
 
+export const productsReducer = createReducer(
+  initialProductsState,
 
-export const coursesReducer = createReducer(
-
-    initialCoursesState,
-
-    on(ProductActions.retrieveProductList,
-        (state, action) => adapter.setAll( // getAll
-            action.products,
-            {...state,
-                allProductsLoaded:true
-            })),
+  //   on(ProductActions.retrieveProductList, (state, action) =>
+  //     adapter.setAll(
+  //       // getAll
+  //       action.products,
+  //       { ...state, allProductsLoaded: true }
+  //     )
+  //   )
+  on(ProductActions.retrieveProductList, (state, payload) =>
+    adapter.addMany(payload.products, state)
+  )
 );
 
+export const { selectAll, selectEntities, selectIds, selectTotal } =
+  adapter.getSelectors();
 
-export const {
-    selectAll
-} = adapter.getSelectors();
+export function compareProducts(c1: Product, c2: Product) {
+  const compare = c1.price - c2.price;
+
+  if (compare > 0) {
+    return 1;
+  } else if (compare < 0) {
+    return -1;
+  } else return 0;
+}
