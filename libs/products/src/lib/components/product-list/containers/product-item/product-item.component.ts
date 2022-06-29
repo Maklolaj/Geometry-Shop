d8ae4fc664520831db0645469649271a56d80948
Maklolaj <m.zelek@op.pl>
2022-06-29 18:33:17 +0200

    conditional btns

--- libs/products/src/lib/components/product-list/containers/product-item/product-item.component.ts
+++ libs/products/src/lib/components/product-list/containers/product-item/product-item.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { Product } from '@geometry-shop/data-models';
 import { compose, select, Store } from '@ngrx/store';
 import {
@@ -9,14 +14,15 @@ import { BasketState } from 'libs/products/src/lib/state/basket/basket.state';
 import { selectCurrentProduct } from 'libs/products/src/lib/state/products/product.selectors';
 import { ProductState } from 'libs/products/src/lib/state/products/product.state';
 import { selectProductId } from 'libs/products/src/lib/state/products/products.actions';
-import { take } from 'rxjs';
+import { Observable, of, take, mergeMap, map } from 'rxjs';
+import { selectAllBasketProducts } from '../../../../state/basket/basket.selectors';
 
 @Component({
   selector: 'geometry-shop-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss'],
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
   //TODO change detection on push?
   @Input() product!: Product;
   constructor(
@@ -24,8 +30,15 @@ export class ProductItemComponent {
     private readonly basketStore: Store<BasketState>
   ) {}
 
+  isInBasket: Observable<boolean> = of(false);
+
+  ngOnInit(): void {
+    this.isInBasket = this.basketStore
+      .pipe(select(selectAllBasketProducts))
+      .pipe(map((basketList: Product[]) => !basketList.indexOf(this.product)));
+  }
+
   public showProduct(productId: string): void {
-    console.log(productId);
     this.productStore.dispatch(selectProductId({ productId }));
   }
 
