6b622f734f8eaafa26c9a255b270a616ac93e9da
Maklolaj <m.zelek@op.pl>
2022-07-19 22:39:26 +0200

    add summary table

--- libs/summary/src/lib/containers/basket-summary/basket-summary.component.ts
+++ libs/summary/src/lib/containers/basket-summary/basket-summary.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '@geometry-shop/data-models';
+import { BasketState } from 'libs/products/src/lib/state/basket/basket.state';
+import { select, Store } from '@ngrx/store';
+import { selectAllBasketProducts } from '../../../../../products/src/lib/state/basket/basket.selectors';
+import { Observable, of, take } from 'rxjs';
 
 @Component({
   selector: 'geometry-shop-basket-summary',
@@ -6,7 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basket-summary.component.scss'],
 })
 export class BasketSummaryComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  basketItems: Observable<Product[]> = of([]);
+
+  public totalCost: number = 0;
+
+  constructor(private readonly basketStore: Store<BasketState>) {}
+  
+  ngOnInit(): void {
+    this.basketItems = this.basketStore
+      .pipe(select(selectAllBasketProducts))
+    this.basketItems.pipe(take(1)).subscribe( (products:Product[]) => {
+      this.totalCost = products.map(t => t.price).reduce((acc, value) => acc + value, 0);
+    })
+  }
+
+  columns = [
+    {
+      columnDef: 'name',
+      header: 'Name',
+      cell: (element: Product) => `${element.name}`,
+    },
+    {
+      columnDef: 'price',
+      header: 'Price',
+      cell: (element: Product) => `${element.price}`,
+    },
+  ];
+  displayedColumns = this.columns.map((c) => c.columnDef);
 }
