d0959e6dcdf3fd4b253e8da447635f332e5369b8
Maklolaj <m.zelek@op.pl>
2022-08-14 11:19:03 +0200

    add userProfile reducer

--- libs/data-access/src/lib/state/userProfile/userProfile.reducer.ts
+++ libs/data-access/src/lib/state/userProfile/userProfile.reducer.ts
@@ -1,6 +1,8 @@
+import { Action, combineReducers } from '@ngrx/store';
 import {
-  createFormGroupState,
+  createFormGroupState, createFormStateReducerWithUpdate, FormGroupState, updateGroup, validate,
 } from 'ngrx-forms';
+import { required, requiredTrue, maxLength } from 'ngrx-forms/validation';
 
 export interface FormValues {
     name: string;
@@ -10,12 +12,24 @@ export interface FormValues {
     city: string;
     postalCode: string;
     agreeToTermsOfUse: boolean,
-
 }
+
+export interface UserProfileFormState {
+  userProfile: {
+    formState: FormGroupState<FormValues>;
+    submittedValue: FormValues | undefined;
+  };
+}
+
+export class SetSubmittedValueAction implements Action {
+  static readonly TYPE = 'userProfile/SET_SUBMITTED_VALUE';
+  readonly type = SetSubmittedValueAction.TYPE;
+  constructor(public submittedValue: FormValues) { }
+} 
 
 export const FORM_ID = 'userProfile';
 
-export const initialFormGroupState = createFormGroupState<FormValues>(FORM_ID, {
+export const INITIAL_FORM_GROUP_STATE = createFormGroupState<FormValues>(FORM_ID, {
     name: '',
     surname: '',
     dateOfBirth: new Date(Date.UTC(1970, 0, 1)).toISOString(),
@@ -25,3 +39,27 @@ export const initialFormGroupState = createFormGroupState<FormValues>(FORM_ID, {
     agreeToTermsOfUse: false,
 });
 
+const validationFormReducer = createFormStateReducerWithUpdate<FormValues>(updateGroup<FormValues>({
+  name: validate(required, maxLength(20)),
+  surname: validate(required, maxLength(20)),
+  agreeToTermsOfUse:  validate(requiredTrue),
+}))
+
+const userProfileCombineReducers = combineReducers<UserProfileFormState['userProfile'], any>({
+  formState(s = INITIAL_FORM_GROUP_STATE, a: Action) {
+    return validationFormReducer(s, a);
+  },
+  submittedValue(s: FormValues | undefined, a: SetSubmittedValueAction) {
+    switch (a.type) {
+      case SetSubmittedValueAction.TYPE:
+        return a.submittedValue;
+
+      default:
+        return s;
+    }
+  },
+});
+
+export function userProfileReducers(s: UserProfileFormState['userProfile'], a: Action) {
+  return userProfileCombineReducers(s, a);
+}
